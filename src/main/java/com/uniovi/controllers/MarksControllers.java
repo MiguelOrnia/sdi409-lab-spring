6f4b1d6e1d4b93db60257f19d806b54fec8451e8
MiguelOrnia <mor-nia@hotmail.com>
2019-02-20 17:52:18 +0100

    SDI - SpringBoot - Sesión - 4.1 - Sesiones

--- src/main/java/com/uniovi/controllers/MarksControllers.java
+++ src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -1,5 +1,10 @@
 package com.uniovi.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +25,9 @@ public class MarksControllers {
 
 	@Autowired
 	private UsersService usersService;
+
+	@Autowired
+	private HttpSession httpSession;
 
 	@RequestMapping(value = "/mark/add")
 	public String getMark(Model model) {
