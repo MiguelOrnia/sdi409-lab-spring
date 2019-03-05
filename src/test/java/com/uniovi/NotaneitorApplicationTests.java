31d44dfc3f300c143cdd5b3b43407004f591b1a4
MiguelOrnia <mor-nia@hotmail.com>
2019-03-05 21:54:54 +0100

    SDI - Selenium - 5.1 - ClasePrincipal

--- src/test/java/com/uniovi/NotaneitorApplicationTests.java
+++ src/test/java/com/uniovi/NotaneitorApplicationTests.java
@@ -1,17 +1,73 @@
 package com.uniovi;
 
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runners.MethodSorters;
+import org.junit.FixMethodOrder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//Ordenamos las pruebas por el nombre del método
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas)):
+	// static String PathFirefox64 = "C:\\Program Files\\Mozilla
+	// Firefox\\firefox.exe";
+	// static String Geckdriver022 = "C:\\Path\\geckodriver024win64.exe";
+	// En MACOSX (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas):
+	static String PathFirefox65 = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver024 = "C:\\Users\\Miguel\\Desktop\\PL-SDI-Sesion5-material\\geckodriver024win64.exe";
+	// Común a Windows y a MACOSX
+
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "http://localhost:8090";
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+		
+	// Antes de cada prueba se navega al URL home de la aplicación
+	@Before
+	public void setUp() {
+		driver.navigate().to(URL);
 	}
 
-}
+	// Después de cada prueba se borran las cookies del navegador
+	@After
+	public void tearDown() {
+		driver.manage().deleteAllCookies();
+	}
 
+	// Antes de la primera prueba
+	@BeforeClass
+	static public void begin() {
+	}
+
+	// Al finalizar la última prueba
+	@AfterClass
+	static public void end() {
+		// Cerramos el navegador al finalizar las pruebas
+		driver.quit();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
